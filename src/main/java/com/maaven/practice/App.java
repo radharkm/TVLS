d3f179c8fcf6d34ca0b41bcd5f40d226e8631ab0
krishna771 <krishna.77cwa@gmail.com>
2020-01-06 20:16:46 +0530

    mysis work

---
+++ src/main/java/com/maaven/practice/App.java
@@ -0,0 +1,23 @@
+package com.maaven.practice;
+
+/**
+ * CallingMethodsInSameClass!
+ *
+ */
+public class App
+{
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+}
